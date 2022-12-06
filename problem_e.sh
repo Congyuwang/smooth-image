485c0720c0d2d05abb42d4a2f6a82d8f4a6b95f7
Congyu Wang <116020237@link.cuhk.edu.cn>
2022-12-07 00:18:11 +0800

    add AG run

--- problem_e.sh
+++ problem_e.sh
@@ -38,3 +38,32 @@ run in-paint \
   --init random \
   --tol 0.0001 \
   --mu 0.01
+
+echo Running AG Algorithm
+
+run in-paint \
+  -i test/test_images/512_512_pens.png \
+  -m test/test_masks/640_640_scratches_01.png \
+  -o test/test_outputs/problem-e-pens-ag.png \
+  --algo ag \
+  --init zero \
+  --tol 0.0001 \
+  --mu 0.01
+
+run in-paint \
+  -i test/test_images/1024_1024_bluestreet.png \
+  -m test/test_masks/512_512_handwriting.png \
+  -o test/test_outputs/problem-e-bluestree-ag.png \
+  --algo ag \
+  --init random \
+  --tol 0.0001 \
+  --mu 0.01
+
+run in-paint \
+  -i test/test_images/4096_4096_husky.jpg \
+  -m test/test_masks/512_512_handwriting.png \
+  -o test/test_outputs/problem-e-husky-ag.png \
+  --algo ag \
+  --init random \
+  --tol 0.0001 \
+  --mu 0.01
